987eea5b595259ea2bc75c3cac986c51336316f0
Mark Watson <markw@markwatson.com>
2016-10-09 16:51:15 -0700

    text cleaning example

--- TextProcessing/CleanText.hs
+++ TextProcessing/CleanText.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module TestCleanText where
+module CleanText (cleanText, removeStopWords)  where
 
 import Data.List.Split (splitOn)
 import Data.List (intercalate)
 import Data.Char as C
+import Data.List.Utils (replace)
 
 noiseCharacters = ['[', ']', '{', '}', '\n', '\t', '&', '^', 
                    '@', '%', '$', '#', ',']
@@ -17,9 +18,12 @@ cleanText s =
    " " $
    filter
      (\x -> length x > 0) $
-     splitOn " " $ substituteNoiseCharacters s
+     splitOn " " $ substituteNoiseCharacters $
+       (replace "." " . "
+        (replace "," " , " 
+         (replace ";" " ; " s)))
 
-stopWords = ["a", "the", "that", "of", "an"]
+stopWords = ["a", "the", "that", "of", "an", "and"]
 
 toLower' s = map (\x -> if isLower x then x else (C.toLower x)) s
 
