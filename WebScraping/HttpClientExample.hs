c9e3b49684ed5ed0706e1e7fb6486253b8191777
Mark Watson <markw@markwatson.com>
2016-10-13 15:09:25 -0700

    split web scraping example into 3 separate examples

--- WebScraping/HttpClientExample.hs
+++ WebScraping/HttpClientExample.hs
@@ -1,17 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 
--- example 1 reference: http://www.serpentine.com/wreq/tutorial.html
--- example 2 references: https://github.com/egonSchiele/HandsomeSoup
---                       http://adit.io/posts/2012-04-14-working_with_HTML_in_haskell.html
+-- reference: http://www.serpentine.com/wreq/tutorial.html
 
-module Main where
+module HttpClientExample where
 
 -- for first example:
 import Network.Wreq
 import Control.Lens
-import Data.Aeson.Lens (_String, key)
-import Data.Aeson (decode)
-import Data.ByteString.Lazy hiding(putStrLn)
+--import Data.Aeson.Lens (_String, key)
+--import Data.Aeson (decode)
+--import Data.ByteString.Lazy hiding(putStrLn)
 import Data.Maybe
 
 -- for second example:
@@ -19,37 +17,14 @@ import Text.XML.HXT.Core
 import Text.HandsomeSoup
 
 -- third example:
---import Text.JSON.Generic
+import Text.JSON.Generic
 
 main :: IO ()
 main = do
-  -- first example:
-  r <- get "http://markwatson.com"
-  print $ "status code: " ++ (show (r ^. responseStatus . statusCode))
-  print $ "url: " ++ (show (r ^? responseBody . key "url"))
-  print $ "content type: " ++ (show (r ^? responseHeader "Content-Type"))
-  --print $ "body: " ++ (show (r ^? responseBody))
-  
-  -- second example:
-  let doc = fromUrl "http://markwatson.com/"
-  links <- runX $ doc >>> css "a" ! "href"
-  mapM_ print links
-  h2 <- runX $ doc >>> css "h2" ! "href"
-  mapM_ print h2
-  imageSrc <- runX $ doc >>> css "img" ! "src"
-  mapM_ print imageSrc
-  allBodyText <- runX $ doc >>> css "body" //> getText
-  mapM_ print allBodyText
-  pText <- runX $ doc >>> css "p" //> getText -- //> gets al contained text
-                                              -- /> gets only directly contained text
-  mapM_ print pText
-  
-  -- third example: JSON from DBPedia
+  -- JSON from DBPedia
   r <- get "http://dbpedia.org/data/Sedona_Arizona.json"
-  print $ "status code: " ++ (show (r ^. responseStatus . statusCode))
-  print $ "url: " ++ (show (r ^? responseBody . key "url"))
-  print $ "content type: " ++ (show (r ^? responseHeader "Content-Type"))
-  print $ "json: " ++ show (fromJust (r ^? responseBody))
-  --print $ decode $ (fromJust (r ^? responseBody))
+  putStrLn $ "status code: " ++ (show (r ^. responseStatus . statusCode))
+  putStrLn $ "content type: " ++ (show (r ^? responseHeader "Content-Type"))
+  putStrLn $ "json: " ++ show (fromJust (r ^? responseBody))
   
-  -- note: might want to add:  http://dbpedia.org/data/Sedona_Arizona.n3
+  -- note: same data as N3 RDF:  http://dbpedia.org/data/Sedona_Arizona.n3
