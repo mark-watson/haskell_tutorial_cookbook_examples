d31ff348039e8aad360db16bb19a829538b753ff
Mark Watson <markw@markwatson.com>
2017-12-02 19:32:49 -0600

    added docs to Spec Tests

--- TestingHaskell/test/Spec.hs
+++ TestingHaskell/test/Spec.hs
@@ -12,6 +12,8 @@ spec = do
       head [1,2,3,4] `shouldBe` 1
       head ["the", "dog", "ran"] `shouldBe` "dog" -- should fail
   describe "MyColors tests" $ do
-    it "test custom 'compare' function" $ do
+    it "test custom 'compare' function descending test" $ do
       MyColors.Green < MyColors.Red `shouldBe` True
-      Red > Silver `shouldBe` True
+    it "test custom 'compare' function ascending test" $ do
+      Red > Silver `shouldBe` False
+  
