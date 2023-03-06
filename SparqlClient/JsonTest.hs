b4ddfc7cc0290f4f1617fca9c363260f43ab66cf
Mark Watson <markw@markwatson.com>
2023-03-06 13:47:46 -0700

    added debug

--- SparqlClient/JsonTest.hs
+++ SparqlClient/JsonTest.hs
@@ -4,10 +4,15 @@ import qualified Data.ByteString.Char8 as S8
 import qualified Data.Yaml             as Yaml
 import           Network.HTTP.Simple
 
+import Data.Typeable(typeOf) -- for debugging
+import Control.Monad.Trans(lift) -- experiment only
+
 main :: IO ()
 main = do
 
     response <- httpJSON "http://dbpedia.org/sparql/?query=select * where {?s ?p ?o} limit 2"
+    print $ typeOf response
+--    rb <- lift $ (getResponseBody response) --  :: Value)
     putStrLn $ "The status code was: " ++
                show (getResponseStatusCode response)
     print $ getResponseHeader "Content-Type" response
