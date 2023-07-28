b1ebcff2d83a4eb0e111b06bc642ab88e64eee7b
Mark Watson <markw@markwatson.com>
2023-07-28 14:01:50 -0700

    not in book: trying Alexander Thiemann's library for OpenAI

---
+++ OpenAiApiClient/GenText.hs
@@ -0,0 +1,42 @@
+-- Example from https://github.com/agrafix/openai-hs/tree/main/openai-hs
+-- code by Alexander Thiemann
+
+{-# LANGUAGE OverloadedStrings #-}
+import OpenAI.Client
+
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
+import System.Environment (getEnv)
+import qualified Data.Text as T
+
+request :: ChatCompletionRequest
+request = ChatCompletionRequest 
+         { chcrModel = ModelId "gpt-3.5-turbo"
+         , chcrMessages = 
+            [ChatMessage { chmContent = "Write a hello world program in Haskell"
+                         , chmRole = "user"
+                         }
+            ]
+         , chcrTemperature = Nothing
+         , chcrTopP = Nothing
+         , chcrN = Nothing
+         , chcrStream = Nothing
+         , chcrStop = Nothing
+         , chcrMaxTokens = Nothing
+         , chcrPresencePenalty = Nothing
+         , chcrFrequencyPenalty = Nothing
+         , chcrLogitBias = Nothing
+         , chcrUser = Nothing
+         }
+
+main :: IO ()
+main =
+  do manager <- newManager tlsManagerSettings
+     apiKey <- T.pack <$> getEnv "OPENAI_KEY"
+     -- create a openai client that automatically retries up to 4 times on network
+     -- errors
+     let client = makeOpenAIClient apiKey manager 4
+     result <- completeChat client request        
+     case result of
+       Left failure -> print failure
+       Right success -> print $ chrChoices success
---
+++ OpenAiApiClient/README.md
@@ -0,0 +1,5 @@
+# OpenAI APIs
+
+I am using the library written by Alexander Thiemann at:
+
+    https://github.com/agrafix/openai-hs/tree/main/openai-hs
